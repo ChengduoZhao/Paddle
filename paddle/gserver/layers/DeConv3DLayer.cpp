90db0c2b9f272e987321710511e74c2a0765a1d7
chengduo <zhaochengduo@163.com>
2017-08-10 10:28:54 +0800

    Update DeConv3DLayer.cpp
    Modify variable format

--- paddle/gserver/layers/DeConv3DLayer.cpp
+++ paddle/gserver/layers/DeConv3DLayer.cpp
@@ -24,187 +24,186 @@ REGISTER_LAYER(deconv3d, DeConv3DLayer);
     (((IN_SIZE) - 1) * (STRID) - 2 * (PAD) + (KSIZE))
 
 bool DeConv3DLayer::init(const LayerMap &layerMap,
-                       const ParameterMap &parameterMap) {
-    if (!ConvBaseLayer::init(layerMap, parameterMap)) return false;
-    // for Deconv, the dimension of Kernel is
-    // channel * output * depth * height * weigth
-    // Matrix storage format: (output * depth * height * weigth) x  channel
-    for (int index = 0; index <  config_.inputs().size(); ++index) {
-        M_.push_back(filterChannels_[index]);
-        K_.push_back(
-                filterPixels_[index] * (numFilters_/groups_[index]));
-        weights_[index]->getW()->reshape(
-                filterPixels_[index] * numFilters_,
-                filterChannels_[index]);
-        weights_[index]->getWGrad()->reshape(
-                filterPixels_[index] * numFilters_,
-                filterChannels_[index]);
-    }
-    CHECK(inputLayers_.size() == parameters_.size());
-    return true;
+                     const ParameterMap &parameterMap) {
+  if (!ConvBaseLayer::init(layerMap, parameterMap)) return false;
+  // for Deconv, the dimension of Kernel is
+  // channel * output * depth * height * weigth
+  // Matrix storage format: (output * depth * height * weigth) x  channel
+  for (int index = 0; index < config_.inputs().size(); ++index) {
+    M_.push_back(filterChannels_[index]);
+    K_.push_back(
+            filterPixels_[index] * (numFilters_/groups_[index]));
+    weights_[index]->getW()->reshape(
+            filterPixels_[index] * numFilters_,
+            filterChannels_[index]);
+    weights_[index]->getWGrad()->reshape(
+            filterPixels_[index] * numFilters_,
+            filterChannels_[index]);
+  }
+  CHECK(inputLayers_.size() == parameters_.size());
+  return true;
 }
 
 
 size_t DeConv3DLayer::getSize() {
-    CHECK_NE(inputLayers_.size(), 0UL);
-    // imgSizeH_.clear();
-    // imgSizeW_.clear();
-    // imgSizeD_.clear();
-    outputH_.clear();
-    outputW_.clear();
-    outputD_.clear();
-    N_.clear();
-    No_.clear();
-    size_t layerSize = 0;
-    for (size_t i = 0; i < inputLayers_.size(); ++i) {
-        // imgSizeH_.push_back(inputLayers_[i]->getOutput().getFrameHeight());
-        // imgSizeW_.push_back(inputLayers_[i]->getOutput().getFrameWidth());
-        // imgSizeD_.push_back(inputLayers_[i]->getOutput().getFrameDepth());
-        outputW_.push_back(
-                DECONV_OUTPUT_SIZE(
-                        imgSizeW_[i], stride_[i],
-                        padding_[i], filterSize_[i]));
-        outputH_.push_back(
-                DECONV_OUTPUT_SIZE(
-                        imgSizeH_[i], strideY_[i],
-                        paddingY_[i], filterSizeY_[i]));
-        outputD_.push_back(
-                DECONV_OUTPUT_SIZE(
-                        imgSizeD_[i], strideZ_[i],
-                        paddingZ_[i], filterSizeZ_[i]));
-        No_.push_back(outputD_[i] * outputH_[i] * outputW_[i]);
-        N_.push_back(imgSizeD_[i] * imgSizeH_[i] * imgSizeW_[i]);
-        CHECK(layerSize == 0 || N_[i] * size_t(numFilters_) == layerSize);
-        layerSize += No_[i] * numFilters_;
-    }
-    getOutput().setFrameHeight(outputH_[0]);
-    getOutput().setFrameWidth(outputW_[0]);
-    getOutput().setFrameDepth(outputD_[0]);
-    return layerSize;
+  CHECK_NE(inputLayers_.size(), 0UL);
+  // imgSizeH_.clear();
+  // imgSizeW_.clear();
+  // imgSizeD_.clear();
+  outputH_.clear();
+  outputW_.clear();
+  outputD_.clear();
+  N_.clear();
+  No_.clear();
+  size_t layerSize = 0;
+  for (size_t i = 0; i < inputLayers_.size(); ++i) {
+    // imgSizeH_.push_back(inputLayers_[i]->getOutput().getFrameHeight());
+    // imgSizeW_.push_back(inputLayers_[i]->getOutput().getFrameWidth());
+    // imgSizeD_.push_back(inputLayers_[i]->getOutput().getFrameDepth());
+    outputW_.push_back(
+            DECONV_OUTPUT_SIZE(
+                    imgSizeW_[i], stride_[i],
+                    padding_[i], filterSize_[i]));
+    outputH_.push_back(
+            DECONV_OUTPUT_SIZE(
+                    imgSizeH_[i], strideY_[i],
+                    paddingY_[i], filterSizeY_[i]));
+    outputD_.push_back(
+            DECONV_OUTPUT_SIZE(
+                    imgSizeD_[i], strideZ_[i],
+                    paddingZ_[i], filterSizeZ_[i]));
+    No_.push_back(outputD_[i] * outputH_[i] * outputW_[i]);
+    N_.push_back(imgSizeD_[i] * imgSizeH_[i] * imgSizeW_[i]);
+    CHECK(layerSize == 0 || N_[i] * size_t(numFilters_) == layerSize);
+    layerSize += No_[i] * numFilters_;
+  }
+  getOutput().setFrameHeight(outputH_[0]);
+  getOutput().setFrameWidth(outputW_[0]);
+  getOutput().setFrameDepth(outputD_[0]);
+  return layerSize;
 }
 
 void DeConv3DLayer::forward(PassType passType) {
-    Layer::forward(passType);
-    int batchSize = inputLayers_[0]->getOutputValue()->getHeight();
-    int outWidth = getSize();
-    resetOutput(batchSize, outWidth);
-    const MatrixPtr output = getOutputValue();
+  Layer::forward(passType);
+  int batchSize = inputLayers_[0]->getOutputValue()->getHeight();
+  int outWidth = getSize();
+  resetOutput(batchSize, outWidth);
+  const MatrixPtr outMat = getOutputValue();
 
-    for (size_t i = 0; i != inputLayers_.size(); ++i) {
-        REGISTER_TIMER_INFO("FwdDeConv3D", getName().c_str());
-        const MatrixPtr& input = getInputValue(i);
-        int width = input->getWidth();
-        int M = M_[i];
-        int N = N_[i];
-        int K = K_[i];
-        MatrixPtr weights = weights_[i]->getW();
-        Matrix::resizeOrCreate(colBuf_, K * groups_[i] , N, false, useGpu_);
+  for (size_t i = 0; i != inputLayers_.size(); ++i) {
+    REGISTER_TIMER_INFO("FwdDeConv3D", getName().c_str());
+    const MatrixPtr& inMat = getInputValue(i);
+    int width = inMat->getWidth();
+    int M = M_[i];
+    int N = N_[i];
+    int K = K_[i];
+    MatrixPtr wMat = weights_[i]->getW();
+    Matrix::resizeOrCreate(colBuf_, K * groups_[i] , N, false, useGpu_);
 
-        for (int n = 0; n < batchSize; ++n) {
-            real *inData = input->getData() + n * width;
-            real *colBufData = colBuf_->getData();
-            for (int g = 0; g < groups_[i]; g++) {
-               MatrixPtr weight = weights->subMatrix(g * K, K);  // K * M
-               MatrixPtr in =
-                       Matrix::create(inData, M, N, false, useGpu_);
-               MatrixPtr sub_colBufData =
-                       Matrix::create(colBufData, K, N, false, useGpu_);
-               sub_colBufData->mul(*weight, *in, 1, 0);
-               colBufData += K * N;
-               inData += M * N;
-            }
-            colBuf_->col2Vol(output->getData()+ n * output->getWidth(),
-                             numFilters_, outputD_[i], outputH_[i], outputW_[i],
-                             filterSizeZ_[i], filterSizeY_[i], filterSize_[i],
-                             strideZ_[i], strideY_[i], stride_[i],
-                             paddingZ_[i], paddingY_[i], padding_[i], 1.0, 1.0);
-        }
+    for (int n = 0; n < batchSize; ++n) {
+      real *inData = inMat->getData() + n * width;
+      real *colBufData = colBuf_->getData();
+      for (int g = 0; g < groups_[i]; g++) {
+         MatrixPtr wMatSub = wMat->subMatrix(g * K, K);
+         MatrixPtr inMatSub =
+                 Matrix::create(inData, M, N, false, useGpu_);
+         MatrixPtr colBufDataSub =
+                 Matrix::create(colBufData, K, N, false, useGpu_);
+         colBufDataSub->mul(*wMatSub, *inMatSub, 1, 0);
+         colBufData += K * N;
+         inData += M * N;
+      }
+      colBuf_->col2Vol(outMat->getData()+ n * outMat->getWidth(),
+                       numFilters_, outputD_[i], outputH_[i], outputW_[i],
+                       filterSizeZ_[i], filterSizeY_[i], filterSize_[i],
+                       strideZ_[i], strideY_[i], stride_[i],
+                       paddingZ_[i], paddingY_[i], padding_[i], 1.0, 1.0);
     }
-    if (nullptr != this->biasParameter_) {
-        REGISTER_TIMER_INFO("FwBiasTimer", getName().c_str());
-        this->addBias();
-    }
-    forwardActivation();
+  }
+  if (nullptr != this->biasParameter_) {
+    REGISTER_TIMER_INFO("FwBiasTimer", getName().c_str());
+    this->addBias();
+  }
+  forwardActivation();
 }
 
 void DeConv3DLayer::backward(const UpdateCallback &callback) {
-    backwardActivation();
-    int batchSize = getOutputGrad()->getHeight();
-    int out_width = getOutputGrad()->getWidth();
-    if (biases_ && biases_->getWGrad()) {
-        bpropBiases();
-        biases_->getParameterPtr()->incUpdate(callback);
-    }
-    for (int i =0; i < inputLayers_.size(); ++i) {
-        int M = M_[i];
-        int N = N_[i];
-        int K = K_[i];
-        Matrix::resizeOrCreate(colBuf_, K * groups_[i], N, false, useGpu_);
-        real* outGrad = getOutputGrad()->getData();
-        for (int n = 0; n < batchSize; ++n) {
-            REGISTER_TIMER_INFO("BwdDeConv3D", getName().c_str());
-            if (weights_[i]->getWGrad() || this->needGradient_) {
-                colBuf_->vol2Col(outGrad + n * out_width, numFilters_,
-                             outputD_[i], outputH_[i], outputW_[i],
-                             filterSizeZ_[i], filterSizeY_[i], filterSize_[i],
-                             strideZ_[i], strideY_[i], stride_[i],
-                             paddingZ_[i], paddingY_[i], padding_[i]);
-            }
-            if (weights_[i]->getWGrad()) {
-                const MatrixPtr& input = getInputValue(i);
-                real *inData = input->getData() + n * input->getWidth();;
-                real *WGradData = weights_[i]->getWGrad()->getData();
-                for (int g = 0; g < groups_[i]; g++) {
-                    MatrixPtr sub_colBufData = colBuf_->subMatrix(g * K, K);
-                    MatrixPtr in = Matrix::create(
-                            inData, M, N, false, useGpu_);
-                    MatrixPtr WGradSub = Matrix::create(
-                            WGradData, K, M, false, useGpu_);
-                    WGradSub->mul(*sub_colBufData, *(in->getTranspose()), 1, 1);
-                    WGradData += K * M;
-                    inData += M * N;
-                }
-                weights_[i]->getParameterPtr()->incUpdate(callback);
-            }
-            if (this->needGradient_) {
-                real* preGrad = getInputGrad(i)->getData();
-                for (int g = 0; g < groups_[i]; ++g) {
-                    MatrixPtr w = weights_[i]->getW()->subMatrix(g * K, K);
-                    MatrixPtr outG = colBuf_->subMatrix(g * K, K);
-                    MatrixPtr inGrad = Matrix::create(
-                            preGrad, M, N, false, useGpu_);  // M * N
-                    inGrad->mul(*(w->getTranspose()), *outG, 1, 0);
-                    preGrad += M * N;
-                }
-            }
-            REGISTER_TIMER_INFO("WeightUpdate", getName().c_str());
+  backwardActivation();
+  int batchSize = getOutputGrad()->getHeight();
+  int outputWidth = getOutputGrad()->getWidth();
+  if (biases_ && biases_->getWGrad()) {
+    bpropBiases();
+    biases_->getParameterPtr()->incUpdate(callback);
+  }
+  for (size_t i =0; i < inputLayers_.size(); ++i) {
+    int M = M_[i];
+    int N = N_[i];
+    int K = K_[i];
+    Matrix::resizeOrCreate(colBuf_, K * groups_[i], N, false, useGpu_);
+    const MatrixPtr& inMat = getInputValue(i);
+    for (int n = 0; n < batchSize; ++n) {
+      REGISTER_TIMER_INFO("BwdDeConv3D", getName().c_str());
+      if (weights_[i]->getWGrad() || this->needGradient_) {
+        colBuf_->vol2Col(getOutputGrad()->getData() + n * outputWidth,
+                     numFilters_, outputD_[i], outputH_[i], outputW_[i],
+                     filterSizeZ_[i], filterSizeY_[i], filterSize_[i],
+                     strideZ_[i], strideY_[i], stride_[i],
+                     paddingZ_[i], paddingY_[i], padding_[i]);
+      }
+      if (weights_[i]->getWGrad()) {
+        real *inData = inMat->getData() + n * inMat->getWidth();;
+        real *wGradData = weights_[i]->getWGrad()->getData();
+        for (int g = 0; g < groups_[i]; g++) {
+          MatrixPtr colBufDataSub = colBuf_->subMatrix(g * K, K);
+          MatrixPtr inMatSub = Matrix::create(
+                  inData, M, N, false, useGpu_);
+          MatrixPtr wGradMatSub = Matrix::create(
+                  wGradData, K, M, false, useGpu_);
+          wGradMatSub->mul(*colBufDataSub, *(inMatSub->getTranspose()), 1, 1);
+          wGradData += K * M;
+          inData += M * N;
         }
+        weights_[i]->getParameterPtr()->incUpdate(callback);
+      }
+      if (this->needGradient_) {
+        real* preGrad = getInputGrad(i)->getData();
+        for (int g = 0; g < groups_[i]; ++g) {
+          MatrixPtr w = weights_[i]->getW()->subMatrix(g * K, K);
+          MatrixPtr outGradMat = colBuf_->subMatrix(g * K, K);
+          MatrixPtr inGradMatSub = Matrix::create(
+                  preGrad, M, N, false, useGpu_);
+          inGradMatSub->mul(*(w->getTranspose()), *outGradMat, 1, 0);
+          preGrad += M * N;
+        }
+      }
+      REGISTER_TIMER_INFO("WeightUpdate", getName().c_str());
     }
+  }
 }
 
 void DeConv3DLayer::bpropWeights(int i) { }
 void DeConv3DLayer::bpropData(int i) {  }
 
 void DeConv3DLayer::bpropBiases() {
-    MatrixPtr outGrad = getOutputGrad();
-    biases_->getWGrad()->reshape(
-            biases_->getWGrad()->width_, biases_->getWGrad()->height_);
+  MatrixPtr outGradMat = getOutputGrad();
+  biases_->getWGrad()->reshape(
+          biases_->getWGrad()->width_, biases_->getWGrad()->height_);
 
-    if (this->sharedBiases_) {
-        biases_->getWGrad()->collectSharedBias(*outGrad, 1.0f);
-    } else {
-        biases_->getWGrad()->collectBias(*outGrad, 1.0f);
-    }
+  if (this->sharedBiases_) {
+    biases_->getWGrad()->collectSharedBias(*outGradMat, 1.0f);
+  } else {
+    biases_->getWGrad()->collectBias(*outGradMat, 1.0f);
+  }
 }
 
 void DeConv3DLayer::addBias() {
-    MatrixPtr output = getOutputValue();
-    biases_->getW()->reshape(biases_->getW()->width_, biases_->getW()->height_);
-    if (this->sharedBiases_) {
-        output->addSharedBias(*(biases_->getW()), 1.0f);
-    } else {
-        output->addBias(*(biases_->getW()), 1.0f);
-    }
+  MatrixPtr outMat = getOutputValue();
+  biases_->getW()->reshape(biases_->getW()->width_, biases_->getW()->height_);
+  if (this->sharedBiases_) {
+    outMat->addSharedBias(*(biases_->getW()), 1.0f);
+  } else {
+    outMat->addBias(*(biases_->getW()), 1.0f);
+  }
 }
 
 }  // namespace paddle
